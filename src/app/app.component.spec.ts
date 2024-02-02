5eae1a0f1f9ff05efdddba506e6ccb4ac0b41b14
admin <historie.ania@gmail.com>
2024-02-02 12:15:24 +0100

    Lecture 24 : -selectors intro;

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,6 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-first-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    // @ts-ignore
     expect(app.title).toEqual('my-first-app');
   });
 
