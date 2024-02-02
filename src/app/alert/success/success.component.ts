e54156371f889f003c716e937a92baed5970f514
admin <historie.ania@gmail.com>
2024-02-02 15:55:21 +0100

    Assigment 1 -created success alert css, ts and html file -created warning alert css, ts and html file -imported sa and wa into main app

---
+++ src/app/alert/success/success.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'success-alert',
+  templateUrl : './success.component.html',
+  styleUrl: './success.component.css'
+
+})
+
+export class SuccessAlertComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import {HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import {SuccessAlertComponent} from "./alert/success/success.component";
+import {WarningAlertComponent} from "./alert/warning/warning.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
