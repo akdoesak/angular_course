7ebd6b155cf03f17d1674878d2d3e1d5bdc3865f
admin <historie.ania@gmail.com>
2024-02-02 21:46:19 +0100

    Lecutres 27-29 Property Binding and String Interpolation added

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,11 +5,20 @@ import { Component } from '@angular/core';
   //selector: '[app-servers]',
   //selector: '.app-servers',
 
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+ // template: `
+   // <app-server></app-server>
+   // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+   setTimeout(()=>{
+     this.allowNewServer = true;
+   }, 2000)
+  }
+
 
 }
